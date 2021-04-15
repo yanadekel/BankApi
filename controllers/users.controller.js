4d8856404ef3f6c7ace6ae73ce4c73087f7dfd77
yanadekel <75662763+yanadekel@users.noreply.github.com>
2021-04-16 01:23:37 +0300

    add get method

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,20 +1,19 @@
-// const users = require('../users.json');
-const users = [
-  {
-    "passport": 1234543,
-    "cash": 0,
-    "credit": 0
-  }
-]
-
-
+const users = require('../users.json');
+const fs = require('fs');
+// const users = [
+//   {
+//     "passport": 1234543,
+//     "cash": 0,
+//     "credit": 0
+//   }
+// ]
 
 
 
 const addUser = (req, res) => {
   const { passport, cash, credit } = req.body;
 
-  let result = users.find((u) => {
+  let result = users.users.find((u) => {
     return u.passport == passport;
   })
 
@@ -32,15 +31,63 @@ const addUser = (req, res) => {
 
   const usersjson = {
     passport,
-    cash: cash,
-    credit: credit
+    cash,
+    credit,
   }
 
-  users.push(usersjson);
+
+  fs.readFile("./users.json", 'utf8', function readFileCallback(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.write('File not found');
+    } else {
+      // users = JSON.parse(data);
+      users.users.push(usersjson.data);
+      json = JSON.stringify(users); 
+      fs.writeFile("./users.json", json);
+    }
+  });
+
+
+
+
+
   return res.status(200).json({ usersjson: usersjson })
+
 }
+
+
+const getUsers = (req, res) => {
+  fs.readFile("./users.json", null, function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(users));
+    }
+
+  })
+}
+
+const deletUsers = (req, res)=> {
+  const {} = req.body;
+  
+
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   addUser,
+  getUsers,
+  deletUsers
 
 }
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller');
+const fs = require('fs');
 
 
 router.post('/', (req, res) => {
   usersController.addUser(req, res);
   
-})
+}).get('/', (req, res) => {
+  usersController.getUsers(req, res);
+}).delete('/',  (req, res)=>{
+  usersController.deletUsers(req, res);
+} )
 
 
 
